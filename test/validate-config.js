89f5c13da3fda03da0e9673e1f1a30b9e88375a9
atjn <dev@atjn.dk>
2021-05-11 23:32:12 +0200

    Update test dependencies
    Fixes several upstrream security issues

--- test/validate-config.js
+++ test/validate-config.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const t = require("tap");
+const tap = require("tap");
 
 module.exports = async () => {
 
@@ -8,42 +8,42 @@ module.exports = async () => {
 	const linter = new ESLint({"ignore": false});
 	const formatter = await linter.loadFormatter("json");
 
-	t.test("Lint valid file", async t => {
+	tap.test("Lint valid file", async () => {
 
-		t.test("should return a report with zero linting errors", async t => {
+		tap.test("should return a report with zero linting errors", async () => {
 
 			const results = JSON.parse(await formatter.format(await linter.lintFiles(["test/fixture/valid.js"])));
 			
-			t.equal(results[0].errorCount, 0, "reports no errors");
-			t.equal(results[0].warningCount, 0, "reports no warnings");
+			tap.equal(results[0].errorCount, 0, "reports no errors");
+			tap.equal(results[0].warningCount, 0, "reports no warnings");
 
-			t.equal(results[0].messages.length, 0, "reports no messages");
+			tap.equal(results[0].messages.length, 0, "reports no messages");
 
-			t.done();
+			tap.end();
 
 		});
 
-		t.done();
+		tap.end();
 
 	});
 
-	t.test("Lint file with bad code style", async () => {
+	tap.test("Lint file with bad code style", async () => {
 
-		t.test("should return a list of linting errors", async t => {
+		tap.test("should return a list of linting errors", async () => {
 
 			const results = JSON.parse(await formatter.format(await linter.lintFiles(["test/fixture/invalid.js"])));
 			
-			t.ok(results[0].errorCount > 0, "reports at least one error");
+			tap.ok(results[0].errorCount > 0, "reports at least one error");
 
 			for(const error of results[0].messages){
-				t.notEqual(error.fatal, true, "error is not a fatal error (fatal = something else is wrong)");
+				tap.not(error.fatal, true, "error is not a fatal error (fatal = something else is wrong)");
 			}
 
-			t.done();
+			tap.end();
 
 		});
 
-		t.done();
+		tap.end();
 
 	});
 
